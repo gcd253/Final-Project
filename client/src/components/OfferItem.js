9a1b7857919038292991cc353abefb359d37f243
Griffin Dooley <dooleygriffin@gmail.com>
2022-08-16 14:53:41 -0400

    styled the edit and delete posts, now on to routing them

--- client/src/components/OfferItem.js
+++ client/src/components/OfferItem.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ItemCard from './ItemCard'
+import UserPosts from './UserPosts'
 
 const OfferItem = ({ data }) => {
   return (
     <div className="text-gray-600 grid md:grid-cols-3 gap-10 p-14" >
-      {data.map(item => <ItemCard
+      {data.map(item => <UserPosts
         key={item.id}
         id={item.id}
         name={item.name}
---
+++ client/src/components/UserPosts.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const UserPosts = ({ name, des, condition, category, id, imageUrl }) => {
+
+    const pencilIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+    </svg>
+    const binIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+    </svg>
+
+    function deletePost() {
+        console.log(`delete me, my id is ${id}`)
+    }
+
+    function editPost() {
+        console.log(`edit me, my id is ${id}`)
+    }
+
+    return (
+        <div>
+            <div className="bg-gray-100 rounded overflow-hidden shadow-md cursor-pointer duration-500 hover:bg-gray-200 relative pb-12">
+                <img src={imageUrl} alt="an image of the item" ></img>
+                <h2 className="text-gray-800 font-bold">{name}</h2>
+                <h3>{des}</h3>
+                <div className="rounded-full text-xs text-gray-100 pt-1 pb-1 ml-6 mb-2 bottom-0 left-0 bg-gray-400 w-1/3 absolute duration-300 hover:scale-[1.02] hover:bg-gray-500" >
+                    <button onClick={editPost} >{pencilIcon}</button>
+                </div>
+                <div className="rounded-full text-xs text-gray-100 pt-1 pb-1 mr-6 mb-2 bottom-0 right-0 bg-red-400 w-1/3 absolute duration-300 hover:scale-[1.02] hover:bg-red-500" >
+                    <button onClick={deletePost}>{binIcon}</button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default UserPosts
+
