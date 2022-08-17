0e0b0e7f390bbfbe43d684abc99728625c6a3b6c
Griffin Dooley <dooleygriffin@gmail.com>
2022-08-17 11:30:58 -0400

    edit post is now a form that returns with the details

--- client/src/components/EditPost.js
+++ client/src/components/EditPost.js
@@ -2,16 +2,48 @@ import React from 'react'
 
 const EditPost = ({ editCard }) => {
     return (
-        <div>
-            <div className="bg-gray-100 rounded overflow-hidden shadow-md cursor-pointer duration-500 hover:bg-gray-300 hover:scale-[1.03] relative">
-                <h2 className="text-gray-800 font-bold">{editCard.name}</h2>
-                <h3>{editCard.description}</h3>
-                <p>Category: {editCard.category}</p>
-                <p>Condition: {editCard.condition} </p>
-                <button>
-                    {/* Message user {selectedCard.user.username} */}
+        <div className="h-full flex justify-center items-center">
+            <form className="flex w-2/3 flex-col justify-center items-center bg-gray-300 rounded-lg" >
+                <label>Item name:</label>
+                <input
+                    className="block border-none p-1 rounded-lg w-2/3"
+                    type="text"
+                    name="name"
+                    //onChange={handlePostDetailsChange}
+                    value={editCard.name}
+                />
+
+                <label>Description:</label>
+                <textarea
+                    className="block border-none p-1 rounded-lg w-2/3 h-24"
+                    name="description"
+                    rows={5}
+                    //onChange={handlePostDetailsChange}
+                    value={editCard.description}
+                />
+
+                <label>Condition:</label>
+                <input
+                    className="block border-none p-1 rounded-lg w-2/3"
+                    type="text"
+                    name="condition"
+                    //onChange={handlePostDetailsChange}
+                    value={editCard.condition}
+                />
+
+                <label>Category:</label>
+                <input
+                    className="block border-none p-1 rounded-lg w-2/3"
+                    type="text"
+                    name="category"
+                    //onChange={handlePostDetailsChange}
+                    value={editCard.category}
+                />
+
+                <button className="border-none p-1 uppercase rounded-2xl font-bold bg-blue-600 m-4" type="submit" value="Post">
+                    Edit
                 </button>
-            </div>
+            </form>
         </div>
     )
 }
