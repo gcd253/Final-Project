6ce295920033a0c3809c86847cfadad11d77d7aa
Griffin Dooley <dooleygriffin@gmail.com>
2022-08-25 15:03:19 -0400

    presentation

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -6,7 +6,7 @@ const Landing = () => {
     <>
 
       <div className="flex flex-col dark:text-slate-200 dark:bg-slate-900 justify-center items-center m-auto w-4/5">
-        <h1 className="text-3xl mt-14" >Title</h1>
+        <h1 className="text-3xl mt-14" ></h1>
         <h3 className="mt-4" >About this project</h3>
         <p className="bg-gray-200 dark:bg-slate-800 px-4 py-2 my-4 rounded-md" >This project is loosely inspired by a delightful and bemusing situation which is all too common in city life not to have a name, yet I have never heard a term coined for it. Say you're moving out, participating in a bit of spring cleaning, or—god forbid—purging the final remnants of your most recent live-in ex. You've come to the last few items, all of them perfectly useful, but you no longer have any need for them. Sure, you could go through the trouble of trying to sell them; but your sudden urge to redecorate won't last forever, and who in their right mind would want to buy a half-empty bag of paperclips and a single Tupperware?
           <br></br>
